ce61faff3508a99035acb254ec63efbe63e2a713
Derek Baker <derek.baker89@gmail.com>
2021-06-23 12:05:03 +0100

    added teaser test

--- spec/unit/use_case/create_word_spec.rb
+++ spec/unit/use_case/create_word_spec.rb
@@ -1,11 +1,11 @@
 describe UseCase::CreateWord do
     TEST_WORD = "hello"
-    it "creates a new word" do
+    TEST_TEASER = "a greeting"
+    it "creates a new word with a teaser" do
         use_case = described_class.new
-        use_case.execute(TEST_WORD, "a greeting")
+        use_case.execute(TEST_WORD, TEST_TEASER)
 
         expect(Db::Connector.table(:words).where(word: TEST_WORD).to_a).not_to be_empty
-
-        
+        expect(Db::Connector.table(:words).where(word: TEST_WORD).first[:teaser]).to eq TEST_TEASER
     end
 end
