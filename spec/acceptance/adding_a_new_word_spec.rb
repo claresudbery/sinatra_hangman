b642a7860983042ea9a456c5a38c0c497652377a
Derek Baker <derek.baker89@gmail.com>
2021-07-07 11:44:41 +0100

    updated acceptance test with constant

--- spec/acceptance/adding_a_new_word_spec.rb
+++ spec/acceptance/adding_a_new_word_spec.rb
@@ -1,10 +1,11 @@
 describe "acceptance: words: creating" do
+    TEST_WORD = "cricket"
     include RSpecMixin
     let(:get_response) do
         get "/"
     end
     let(:post_response) do
-        post "/admin/word", :word => "meaning"
+        post "/admin/word", :word => TEST_WORD
     end
 
     it "returns a 200" do
@@ -12,7 +13,7 @@ describe "acceptance: words: creating" do
     end
 
     it "shows new word when user adds to database" do
-        expect(post_response.body).to include("meaning")
+        expect(post_response.body).to include(TEST_WORD)
     end
 
     it "shows no word on get" do
