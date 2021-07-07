ef635c398f66c5baeaa9bf9e46e94feb1077ddc6
Derek Baker <derek.baker89@gmail.com>
2021-07-07 11:47:43 +0100

    added teaser constant to acceptance test

--- spec/acceptance/adding_a_new_word_spec.rb
+++ spec/acceptance/adding_a_new_word_spec.rb
@@ -1,11 +1,12 @@
 describe "acceptance: words: creating" do
     TEST_WORD = "cricket"
+    TEST_TEASER = "a sport with a bat and ball"
     include RSpecMixin
     let(:get_response) do
         get "/"
     end
     let(:post_response) do
-        post "/admin/word", :word => TEST_WORD
+        post "/admin/word", :word => TEST_WORD, :teaser => TEST_TEASER
     end
 
     it "returns a 200" do
@@ -18,6 +19,10 @@ describe "acceptance: words: creating" do
 
     it "shows no word on get" do
         expect(get_response.body).to include("no word added")
+    end
+
+    it "shows teaser when new word is added" do
+        expect(post_response.body).to include(TEST_TEASER)
     end
 end
 
