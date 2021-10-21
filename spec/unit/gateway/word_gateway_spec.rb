35edf95be4f2a0c062e0427924ffa928d36edc73
Derek Baker <derek.baker89@gmail.com>
2021-10-21 16:47:48 +0100

    refactored test

--- spec/unit/gateway/word_gateway_spec.rb
+++ spec/unit/gateway/word_gateway_spec.rb
@@ -1,11 +1,18 @@
 describe Gateway::WordGateway do
+    before(:each) do
+        @word_gateway = described_class.new 
+    end
     it "can fetch a word at an index" do
-        fetched_word = described_class.new.fetch(4)
+        fetched_word = @word_gateway.fetch(4)
         expect(fetched_word[:word]).to eq "democracy"
     end
 
     it "returns the number of records in the seed" do
-        count = described_class.new.num_words
+        count = @word_gateway.num_words
         expect(count).to eq 11
     end
+
+    # it "throws an exception if user asks for word from an empty table" do
+    #     expect{@word_gateway.fetch(@word_gateway.num_words)}.to raise_error(EmptyTableError)
+    # end
 end
