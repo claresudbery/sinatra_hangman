39181967314e3f22cfc3ae9db91ccf9d30c7fe65
Derek Baker <derek.baker89@gmail.com>
2021-10-21 16:47:18 +0100

    refactored word gateway to initialize with table name

--- lib/gateway/word_gateway.rb
+++ lib/gateway/word_gateway.rb
@@ -1,16 +1,21 @@
 module Gateway
 
     class WordGateway
+
+        def initialize(table_name = :words )
+            @table_name = table_name
+        end
+
         def create(word)
-            Db::Connector.table(:words).insert word: word.word, teaser: word.teaser
+            Db::Connector.table(@table_name).insert word: word.word, teaser: word.teaser
         end
 
         def fetch(index)
-            Db::Connector.table(:words).offset(index).first
+            Db::Connector.table(@table_name).offset(index).first
         end
 
         def num_words()
-            Db::Connector.table(:words).count
+            Db::Connector.table(@table_name).count
         end
     end
     
