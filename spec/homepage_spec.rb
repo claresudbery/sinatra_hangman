f9066891905c0dbde7e8f161abb3aec640c70989
Derek Baker <derek.baker89@gmail.com>
2021-10-21 11:15:15 +0100

    updated response variable

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -1,13 +1,13 @@
 describe "homepage" do
     include RSpecMixin
     include Rack::Test::Methods
-    let(:response) {get '/'}
+    let(:default_response) {get '/'}
     it "displays the current date" do
-        expect(response.body).to include Time.new.strftime("%d/%m/%Y")
+        expect(default_response.body).to include Time.new.strftime("%d/%m/%Y")
     end
 
     it "displays game objective to users" do
-        expect(response.body).to include("phrase")
+        expect(default_response.body).to include("phrase")
     end
 end
 
