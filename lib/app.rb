047743750c6949297f17c6b2fca3ed9877089d18
Derek Baker <derek.baker89@gmail.com>
2021-10-21 11:14:56 +0100

    extracted admin screen into method

--- lib/app.rb
+++ lib/app.rb
@@ -5,10 +5,15 @@ class App < Sinatra::Base
     set :erb, escape_html: true
     DEFAULT_WORD = "no word added"
     DEFAULT_TEASER = "no teaser added"
-    get '/' do
+
+    def admin_screen
         @word = DEFAULT_WORD
         @teaser = DEFAULT_TEASER
         erb :index, layout: :layout, locals: {current_date: Time.new.strftime("%d/%m/%Y")}
+    end
+
+    get '/' do
+        admin_screen
     end
 
     post '/admin/word' do
