ed50d9c4156838a0011956d8492cea2a387d043a
Derek Baker <derek.baker89@gmail.com>
2021-10-21 14:50:01 +0100

    refactored fetch random word test

--- spec/unit/use_case/play_game_spec.rb
+++ spec/unit/use_case/play_game_spec.rb
@@ -1,16 +1,17 @@
 
 describe UseCase::FetchRandomWord do
-    EXPECTED_WORD_INDEX = 5
-     
-    
 
+    EXPECTED_WORDS = {5 => "democracy",
+        7 => "gambit"
+        }
+    EXPECTED_WORDS.each do |index, word|
+        it "chooses 5th word when randomiser returns 5" do
+            randomiser_stub = double('Randomiser')
+            use_case = described_class.new(randomiser_stub)
+            allow(@randomiser_stub).to receive(:random_int).and_return(index)
+            returned_word = use_case.execute()
     
-    it "chooses a random word for new game" do
-        randomiser_stub = double('Randomiser')
-        use_case = described_class.new(randomiser_stub)
-        allow(@randomiser_stub).to receive(:random_int).and_return(EXPECTED_WORD_INDEX)
-        returned_word = use_case.execute()
-
-        expect(returned_word.word).to eq("democracy")
-    end
+            expect(returned_word.word).to eq(word)
+        end
+    end    
 end
