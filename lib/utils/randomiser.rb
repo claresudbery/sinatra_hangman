a6d38277fa839903ecd9cfc692b8232616e172fb
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-03-18 01:23:09 +0000

    return 0 when 0 passed in

--- lib/utils/randomiser.rb
+++ lib/utils/randomiser.rb
@@ -1,5 +1,9 @@
 class Randomiser
     def random_int(max_int)
-        Kernel.rand(max_int)
+        if max_int == 0
+            0
+        else
+            Kernel.rand(max_int)
+        end
     end
 end
---
+++ spec/unit/utils/randomiser_spec.rb
@@ -0,0 +1,19 @@
+
+require_relative "../../../lib/utils/randomiser"
+
+describe Randomiser do
+    before(:each) do 
+        @randomiser = described_class.new
+    end
+    
+    it "will always return 0 when max_int is 0" do
+        #arrange
+        max_int = 0
+
+        #act
+        random_index = @randomiser.random_int(max_int)
+        
+        #assert
+        expect(random_index).to eq(0)
+    end
+enda
